6fe1519c2ce9d13c93fd67d0d0dd745c1a4cbe29
Jacob <jacobbraslaw@ucsb.edu>
2020-08-24 14:02:28 -0700

    resync

---
+++ R/lab02.R
@@ -0,0 +1,16 @@
+#Jacob
+#08/12/2020
+#lab02
+
+library(tidyverse)
+library(readxl)
+
+url = "..."
+
+homes = read_csv ("data/landdata-states.csv")
+
+Pop <- read_excel("data/PopulationEstimates.xls")
+summary(Pop)
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # geog-176A-labs
+labs will be stored here
